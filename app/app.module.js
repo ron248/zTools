70a51c874189562d78650f29fa9b0687d4ec30c7
ron248 <fackman248@gmail.com>
2015-12-09 15:23:16 +0200

    first

---
+++ app/app.module.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('app', ['ui.bootstrap']);
+
+app.controller('mainController', function($scope){
+    $scope.systems = [
+        {'name':'מערכת', 'imgName': 'panda'},
+        {'name':'מערכת', 'imgName': 'indic'},
+        {'name':'מערכת', 'imgName': 'apollo'},
+        {'name':'מערכת', 'imgName': 'triathlon'},
+        {'name':'מערכת', 'imgName': 'triathlon2'},
+        {'name':'מערכת', 'imgName': 'jupiter'},
+        {'name':'מערכת', 'imgName': 'canary'},
+        {'name':'מערכת', 'imgName': 'hulk'},
+        {'name':'מערכת', 'imgName': 'broadway'},
+        {'name':'מערכת', 'imgName': 'shoev'},
+        {'name':'מערכת', 'imgName': 'date'},
+        {'name':'מערכת', 'imgName': 'termite'},
+        {'name':'מערכת', 'imgName': 'yellow'},
+        {'name':'מערכת', 'imgName': 'crunch'},
+        {'name':'מערכת', 'imgName': 'runway'},
+        {'name':'מערכת', 'imgName': 'childplay'},
+        {'name':'מערכת', 'imgName': 'DMZ'}
+
+    ];
+
+});
